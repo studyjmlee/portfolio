d807d4da3a26cde77c3d4c317acb8f7c103a7d1e
studyjmlee <115996508+studyjmlee@users.noreply.github.com>
2023-05-28 01:31:21 +0900

    2일차

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,48 @@
-$(document).ready(function(){
-    /* 햄버거 btn */
-    $(".hamBtn").click(function(){
-        $(this).toggleClass("active");
-    });
+$(document).ready(function () {
+
+    /* header */
+  function gnbScroll() {
+    var sectionOffset = [];
+    var nowSection = 0;
+    var $section=$(".section-wrap").find(".section");
+    $section.each(function (index, item) {
+        sectionOffset[index] = $(this).offset().top;
+      });
+    for (var i = 0; i < $section.length; i++) {
+      if ($(window).scrollTop() >= sectionOffset[i]) {
+        nowSection = i;
+      }
+    }
+    /* header bgColor 세팅 [S] */
+    /* if($(this).hasClass("type2")){
+                    $(".header-wrap").removeClass("bgBeige");
+                    $(".header-wrap").addClass("bgWhite");
+                }
+                else{
+                    $(".header-wrap").addClass("bgBeige");
+                    $(".header-wrap").removeClass("bgWhite");
+                } */
+    /* header bgColor 세팅 [E] */
+
+    if ($(`.header-list>li`).eq(nowSection).find("a").hasClass("active") == 1) {
+        
+    } else {
+      $(`.header-list>li`).find("a").removeClass("active");
+      $(`.header-list>li`).eq(nowSection).find("a").addClass("active");
+    }
+  }
+
+    /* 초기화 */
+    gnbScroll();
+
+  /* 햄버거 btn */
+  $(".hamBtn").click(function () {
+    $(this).toggleClass("active");
+  });
+
+  
+
+  $(window).on("scroll", function () {
+    gnbScroll();
+  });
 });
