070fe5d6ef6b0882965b686cf219e76c2dcdf2c2
studyjmlee <115996508+studyjmlee@users.noreply.github.com>
2023-05-31 20:02:35 +0900

    about-visual

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,20 +12,77 @@ $(document).ready(function () {
   };
   $.fall("flake",".outside",3,10,120,1,5);
 
+
+  /* letter [S] */
   var controller = new ScrollMagic.Controller();
 
+  
   var scene = new ScrollMagic.Scene({
-    triggerElement: "#cat",
+    triggerElement: ".emphas",
+    /* duration: ".trigger2", */
     triggerHook: "0",
-    /* duration:  "100%"  ,*/ //이 값이 클 수록 천천히 덮어씀
+    /* duration: 3000, */
   })
-    .setClassToggle(".section-wrap", "type2") //트리거가 시작되면 클래스 추가
-    /* .setPin(".intro") */// 트리거에 닿으면 고정시켜줌
-    /* .addIndicators() */ // 기준선
-    .addTo(controller);
+  .setClassToggle(".letter-left", "active")
+    
+  .addTo(controller)
+
+  var controller = new ScrollMagic.Controller();
+
+  
+  var scene = new ScrollMagic.Scene({
+    triggerElement: ".emphas",
+    /* duration: ".trigger2", */
+    triggerHook: "0",
+    /* duration: 3000, */
+  })
+
+  .setClassToggle(".letter-center", "active")
+  .addTo(controller)
+  var controller = new ScrollMagic.Controller();
+
+  
+  var scene = new ScrollMagic.Scene({
+    triggerElement: ".emphas",
+    /* duration: ".trigger2", */
+    triggerHook: "0",
+    /* duration: 3000, */
+  })
+
+  .setClassToggle(".letter-right", "active") 
+  .addTo(controller)
+  /* letter [E] */
+
+  /* about-visual [S] */
+
+  var controller = new ScrollMagic.Controller();
+  
+  var tween1 = new TimelineMax()
+  
+  .fromTo('.visual-img1', 0.15, {y: "100vh"},{y: "-100%"})
+  .fromTo('.visual-img2',0.15,{y: "100vh"},{y: "-100%"})
+/*   .to(".slideContainer", 0.1, { x: "-0%" }) */
+   .to(".slideContainer", 0.1, { x: "-25%" })
+   .to(".slideContainer", 0.1, { x: "-50%" })
+   .to(".slideContainer", 0.1, { x: "-50%" })
+  
+  var scene = new ScrollMagic.Scene({
+    triggerElement: ".about",
+    duration: "500%",
+    triggerHook: "onLeave",
+  })
+  .addIndicators()
+  .setPin(".about")
+  .setTween(tween1)
+  .addTo(controller)
+  
+
+  /* about-visual [E] */
+
 
   $(window).resize(function(){
     $('.outside').snowfall('clear');
     $.fall("flake",".outside",3,10,120,1,5);
   });
 });
+
